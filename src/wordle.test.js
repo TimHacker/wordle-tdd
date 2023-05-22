e8ddda5cdd31174a199fa0ef1e83461ff70a7402
Tim Hacker <tim.hacker@madetech.com>
2023-05-22 15:53:31 +0100

    Added second failing test
    Co-authored-by: Yasmin Morshed <yasmin.morshed@madetech.com>

--- src/wordle.test.js
+++ src/wordle.test.js
@@ -5,3 +5,13 @@ test("it can return five zeros when there are no matches", () => {
   const target = "ropes";
   expect(wordle(guess, target)).toBe("00000");
 });
+
+// Next time:
+// We need to work if that's the best test to learn. (or should we do 00001)
+// Make it green!
+
+test("it can return five twos when everything matches (all the letters are in the correct position", () => {
+  const guess = "ropes";
+  const target = "ropes";
+  expect(wordle(guess, target)).toBe("22222");
+});
