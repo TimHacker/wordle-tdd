bde6ee47c54fbf48573173138c4765e73036cde5
Yasmin Morshed <yasmin.morshed@madetech.com>
2023-06-05 16:19:40 +0100

    Kata solutions, Mon 5 June 23

--- src/wordle.js
+++ src/wordle.js
@@ -1,5 +1,29 @@
 function wordle(guess, target) {
-  return "00000";
+
+  const guessCharacters = guess.split('');
+  const targetCharacters = target.split('');
+
+  let result = "";
+
+  console.log(guessCharacters, targetCharacters)
+
+
+  // Refactor!
+  // or https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
+  // https://stackoverflow.com/questions/9329446/loop-for-each-over-an-array-in-javascript
+  for (let i = 0; i < guessCharacters.length; i++) {
+    let match = targetCharacters[i] === guessCharacters[i];
+    
+    if (match) {
+      result += "2";
+    } else {
+      result += "0";
+    }
+  }
+
+
+
+  return result;
 }
 
 module.exports = wordle;
--- src/wordle.test.js
+++ src/wordle.test.js
@@ -10,16 +10,28 @@ test("it can return five zeros when there are no matches", () => {
 // We need to work if that's the best test to learn. (or should we do 00001)
 // Make it green!
 
-test("it can return five twos when everything matches (all the letters are in the correct position)", () => {
+test("it can return five twos when everything matches (all the correct characters are in the correct position)", () => {
   const guess = "ropes";
   const target = "ropes";
   expect(wordle(guess, target)).toBe("22222");
 });
 
-// Homework
-
-test("it can return four zeros and a one with single character match, wrong position", () => {
+test("it can return four zeros and a two with single character match, correct position (final character)", () => {
   const guess = "ropes";
-  const target = "salty";
-  expect(wordle(guess, target)).toBe("00001");
+  const target = "azyms";
+ expect(wordle(guess, target)).toBe("00002");
 });
+
+// https://blog.codeleak.pl/2021/12/parameterized-tests-with-jest.html
+// it.each([
+//   [[21, 2], 42],
+//   [[3, 7, 2], 42],
+//   [[42, NaN], NaN],
+//   [[42, Infinity], Infinity],
+// ])
+// 
+// test("it can return four zeros and a one with single character match, wrong position", () => {
+//   const guess = "ropes";
+//   const target = "salty";
+//  expect(wordle(guess, target)).toBe("00001");
+// });
